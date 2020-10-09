2e6b8db70e0149da6d8112086019a3c2f765415b
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2020-10-09 17:20:32 +0200

    Feedback 3d

--- src/js/scripts/scrollto.js
+++ src/js/scripts/scrollto.js
@@ -12,7 +12,7 @@ export function scrollto() {
   if ($(window).width() < 992) {
     if (window.location.search.indexOf('scrolltocontent') > -1) {
 	    $('html, body').animate({
-	      scrollTop: $('#content').offset().top - 30
+	      scrollTop: $('#content').offset().top
 	    }, 500, 'swing');
 		} 
   }
